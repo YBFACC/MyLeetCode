0454703b896b662315d061d98dd002b77cabc71a
454447236@qq.com <454447236@qq.com>
2019-04-15 22:45:27 +0800

    ok711

--- easy711/src/teat/teat0415.java
+++ easy711/src/teat/teat0415.java
@@ -1,4 +1,28 @@
 package teat;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 public class teat0415 {
+
+
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = 0; j < J.length(); j++) {
+                if (S.charAt(i) == J.charAt(j)) {
+                    count++;
+                    continue;
+                }
+            }
+        }
+        
+        return count;
+    }
+    public static void main(String[] args){
+        teat0415 a1=new teat0415();
+        a1.numJewelsInStones("aA","aAAbbbb");
+    }
+
+
 }
