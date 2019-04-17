066a8e6ba8b5b71af2e82aee155ea7f0d0b0b38f
454447236@qq.com <454447236@qq.com>
2019-04-17 17:55:32 +0800

    easy231

---
+++ GoLeetCode/src/easy231/test.java
@@ -0,0 +1,20 @@
+package easy231;
+
+public class test {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        for (int i = 0; i < 31; i++) {
+            if (n == 1) return true;
+            if (n >=2) {
+                if (n % 2 != 0) return false;
+                n = n / 2;
+                continue;
+            }
+
+        }
+        return false;
+    }
+
+
+}
