21de5de7110120fc7bd87cd6de73f866751e40cd
454447236@qq.com <454447236@qq.com>
2019-04-21 16:24:17 +0800

    GOGOGO

---
+++ GoLeetCode/src/GoGoGo/aaaa.java
@@ -0,0 +1,25 @@
+package GoGoGo;
+
+public class aaaa {
+    public int missingElement(int[] nums, int k) {
+        int temp = 0;
+        int right = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i] - nums[i - 1]) == 1) {
+                continue;
+            } else {
+                temp += nums[i] - nums[i - 1] - 1;
+            }
+            if (temp < k) {
+                continue;
+            } else {
+                right = nums[i] - temp + k - 1;
+                break;
+            }
+        }
+        if (temp < k) {
+            right = nums[nums.length - 1] + k - temp;
+        }
+        return right;
+    }
+}
