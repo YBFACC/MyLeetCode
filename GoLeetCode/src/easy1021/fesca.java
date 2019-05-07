df1d669ebb51fd253d114b6a2711129967af68ce
454447236@qq.com <454447236@qq.com>
2019-05-07 23:14:47 +0800

    easy1021

--- GoLeetCode/src/easy1021/fesca.java
+++ GoLeetCode/src/easy1021/fesca.java
@@ -2,6 +2,31 @@ package easy1021;
 
 public class fesca {
     public String removeOuterParentheses(String S) {
+        if(S == null || S == "" || S.length() >10000){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        char[] c = S.toCharArray();
+        int temp = 0;
+        int begin = 0;
+        int end = 0;
+        for(int i=0;i<c.length;i++){
+            if(c[i] == '('){
+                if(temp == 0){
+                    begin = i;
+                }
+                temp += 1;
+            }else if(c[i] == ')'){
+                if(temp == 1){
+                    end = i;
+                }
+                temp -= 1;
+            }
 
+            if(temp == 0 && begin < end){
+                sb.append(S.substring(begin+1,end));
+            }
+        }
+        return sb.toString();
     }
 }
