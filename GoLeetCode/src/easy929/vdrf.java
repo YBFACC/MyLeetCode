1febfabeb248aa104c00c86e7dac6d46af69e8b3
454447236@qq.com <454447236@qq.com>
2019-05-08 20:11:45 +0800

    easy929
    已有思路

---
+++ GoLeetCode/src/easy929/vdrf.java
@@ -0,0 +1,12 @@
+package easy929;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class vdrf {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> setemails=new HashSet<>();
+
+    }
+}
