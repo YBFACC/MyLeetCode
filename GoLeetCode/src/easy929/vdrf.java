a33b8ac8a50cac2f26ce32445463bad17919449c
454447236@qq.com <454447236@qq.com>
2019-05-09 15:41:36 +0800

    easy929
    oooooook!!!

--- GoLeetCode/src/easy929/vdrf.java
+++ GoLeetCode/src/easy929/vdrf.java
@@ -1,12 +1,30 @@
 package easy929;
-
-
 import java.util.HashSet;
 import java.util.Set;
 
 public class vdrf {
-    public int numUniqueEmails(String[] emails) {
-        Set<String> setemails=new HashSet<>();
 
+
+
+    public int numUniqueEmails(String[] emails) {
+        Set<String> setemails = new HashSet<>();
+        for (int i = 0; i < emails.length; i++) {
+            String[] str = emails[i].split("@");
+            StringBuilder SB = new StringBuilder("");
+            for (int j = 0; j < str[0].length(); j++) {
+                if (str[0].charAt(j) == '.') {
+                    continue;
+                }
+                if (str[0].charAt(j) == '+') {
+                    break;
+                }
+                SB.append(str[0].charAt(j));
+            }
+            setemails.add(SB.append("@").append(str[1]).toString());
+        }
+        return setemails.size();
     }
+
+
+
 }
