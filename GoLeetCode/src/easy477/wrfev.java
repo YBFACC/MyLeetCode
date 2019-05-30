a6e86ebd353beb28605c4fda37fac92c733c4926
454447236@qq.com <454447236@qq.com>
2019-05-30 17:32:21 +0800

    easy509

--- GoLeetCode/src/easy477/wrfev.java
+++ GoLeetCode/src/easy477/wrfev.java
@@ -2,9 +2,7 @@ package easy477;
 
 public class wrfev {
 
-    public int totalHammingDistance(int[] nums) {
 
-    }
 
 
 }
---
+++ GoLeetCode/src/esay509/frer.java
@@ -0,0 +1,8 @@
+package esay509;
+
+public class frer {
+    public int fib(int N) {
+    if(N==1||N==0) return N;
+    return fib(N-1)+fib(N-2);
+    }
+}
