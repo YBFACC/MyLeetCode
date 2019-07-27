9425c50ed0b9f457c8039c7ce31d7bdc08368fbb
454447236@qq.com <454447236@qq.com>
2019-07-27 14:39:41 +0800

    easy169
    哈希表解题

--- GoLeetCode/src/easy169/scc.java
+++ GoLeetCode/src/easy169/scc.java
@@ -1,6 +1,8 @@
 package easy169;
 
 import java.util.Currency;
+import java.util.HashMap;
+import java.util.Map;
 
 public class scc {
 
@@ -26,25 +28,78 @@ public class scc {
 
 
     //暴力 时间：n^2超限
-    public int majorityElement(int[] nums) {
-        int majorityCount = nums.length/2;
+//    public int majorityElement(int[] nums) {
+//        int majorityCount = nums.length/2;
+//
+//        for (int num : nums) {
+//            int count = 0;
+//            for (int elem : nums) {
+//                if (elem == num) {
+//                    count += 1;
+//                }
+//            }
+//
+//            if (count > majorityCount) {
+//                return num;
+//            }
+//
+//        }
+//
+//        return -1;
+//    }
 
+
+///////////////
+    public static Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
         for (int num : nums) {
-            int count = 0;
-            for (int elem : nums) {
-                if (elem == num) {
-                    count += 1;
-                }
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
             }
-
-            if (count > majorityCount) {
-                return num;
+            else {
+                counts.put(num, counts.get(num)+1);
             }
-
         }
+        return counts;
+    }
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
 
-        return -1;
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+        return majorityEntry.getKey();
     }
 
 
+    public static void main(String[] args) {
+        int [] a={1,3,5,7,9,2,1,2,3,1};
+        System.out.println(majorityElement(a));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
