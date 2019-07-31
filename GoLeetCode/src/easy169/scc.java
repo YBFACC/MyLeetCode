2d80968097209371c6f2a0b5e6035877c484d795
454447236@qq.com <454447236@qq.com>
2019-07-31 16:36:41 +0800

    easy169

--- GoLeetCode/src/easy169/scc.java
+++ GoLeetCode/src/easy169/scc.java
@@ -49,29 +49,92 @@ public class scc {
 //    }
 
 
-///////////////
-    public static Map<Integer, Integer> countNums(int[] nums) {
-        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
-        for (int num : nums) {
-            if (!counts.containsKey(num)) {
-                counts.put(num, 1);
-            }
-            else {
-                counts.put(num, counts.get(num)+1);
-            }
-        }
-        return counts;
-    }
-    public static int majorityElement(int[] nums) {
-        Map<Integer, Integer> counts = countNums(nums);
+///////////////哈希表
+//    public static Map<Integer, Integer> countNums(int[] nums) {
+//        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+//        for (int num : nums) {
+//            if (!counts.containsKey(num)) {
+//                counts.put(num, 1);
+//            }
+//            else {
+//                counts.put(num, counts.get(num)+1);
+//            }
+//        }
+//        return counts;
+//    }
+//    public static int majorityElement(int[] nums) {
+//        Map<Integer, Integer> counts = countNums(nums);
+//
+//        Map.Entry<Integer, Integer> majorityEntry = null;
+//        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+//            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+//                majorityEntry = entry;
+//            }
+//        }
+//        return majorityEntry.getKey();
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        int [] a={1,3,5,7,9,2,1,2,3,1};
+//        System.out.println(majorityElement(a));
+//    }
 
-        Map.Entry<Integer, Integer> majorityEntry = null;
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
-                majorityEntry = entry;
+
+////////////////////////////分治
+//
+//    private int countInRange(int[] nums, int num, int lo, int hi) {
+//        int count = 0;
+//        for (int i = lo; i <= hi; i++) {
+//            if (nums[i] == num) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//
+//    private int majorityE(int[] nums, int lo, int hi) {
+//        // base case; the only element in an array of size 1 is the majority
+//        // element.
+//        if (lo == hi) {
+//            return nums[lo];
+//        }
+//
+//        // recurse on left and right halves of this slice.
+//        int mid = (hi-lo)/2 + lo;
+//        int left = majorityE(nums, lo, mid);
+//        int right = majorityE(nums, mid+1, hi);
+//
+//        // if the two halves agree on the majority element, return it.
+//        if (left == right) {
+//            return left;
+//        }
+//
+//        // otherwise, count each element and return the "winner".
+//        int leftCount = countInRange(nums, left, lo, hi);
+//        int rightCount = countInRange(nums, right, lo, hi);
+//
+//        return leftCount > rightCount ? left : right;
+//    }
+//
+//    public int majorityElement(int[] nums) {
+//        return majorityE(nums, 0, nums.length-1);
+//    }
+
+///////////////////Boyer-Moore 投票算法
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
+            count += (num == candidate) ? 1 : -1;
         }
-        return majorityEntry.getKey();
+
+        return candidate;
     }
 
 
