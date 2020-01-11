2302b60bef1a54c2a6a20b0969a90a9a32d9361c
bingfeng yu <ybf0711@gmail.com>
2020-01-11 18:57:09 +0800

    78-了解下迭代方法

--- JS/78.js
+++ JS/78.js
@@ -50,6 +50,22 @@ var subsets2 = function(nums) {
 }
 
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets3 = function(nums) {
+  let res = [[]];
+  for(let i = 0;i < nums.length;i++){
+      let len = res.length;
+      for(let j = 0;j < len;j++){
+          let sub = res[j].slice();
+          sub.push(nums[i]);
+          res.push(sub);
+      }
+  }
+  return res;
+};
 
-let nums = [-1, 2, 3]
-console.log(new subsets2(nums))
+let nums = [1, 2, 3]
+console.log(new subsets3(nums))
