e5a45cc874ce2e515ddac96f37f64e9a577d86c8
bingfeng yu <ybf0711@gmail.com>
2020-01-12 23:08:28 +0800

    121-一次遍历

--- JS/121.js
+++ JS/121.js
@@ -2,13 +2,31 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
+var maxProfit1 = function(prices) {
   var res = 0
   for (let i = 0; i < prices.length; i++) {
-    for (let j = i+1; j < prices.length; j++) {
-      let temp = prices[j]-prices[i]
+    for (let j = i + 1; j < prices.length; j++) {
+      let temp = prices[j] - prices[i]
       temp > res ? (res = temp) : null
     }
   }
   return res
 }
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  var res = 0
+  var min = prices[0]
+  for (let i = 0; i < prices.length; i++) {
+    prices[i] < min ? (min = prices[i]) : null;
+    prices[i] - min > res ? (res = prices[i] - min) : null
+  }
+
+  return res
+}
+
+var prices = [7, 1, 5, 3, 6, 4]
+maxProfit(prices)
