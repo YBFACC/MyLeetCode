3cbca4a54be725897b784a8daa7e5574ad6eb650
bingfeng yu <ybf0711@gmail.com>
2020-01-13 17:35:23 +0800

    贪心

--- JS/53.js
+++ JS/53.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray1 = function(nums) {
   var res = nums[0]
   for (let i = 0; i < nums.length; i++) {
     let temp = 0
@@ -14,5 +14,20 @@ var maxSubArray = function(nums) {
   return res
 }
 
-var nums=[-2,1,-3,4,-1,2,1,-5,4]
-maxSubArray(nums)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  var res = nums[0]
+  var curr = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    curr=Math.max(nums[i],curr+nums[i])
+    res=Math.max(res,curr)
+  }
+  return res
+}
+
+var nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+console.log(maxSubArray(nums));
+
