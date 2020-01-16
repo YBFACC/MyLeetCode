a09dfa09f99742645e1cdb17da3c6c7a6fb23f45
bingfeng yu <ybf0711@gmail.com>
2020-01-16 20:10:21 +0800

    77-func(temp.slice(), i + 1)=>res.push(temp.slice())---提升时空性能

--- JS/77.js
+++ JS/77.js
@@ -7,12 +7,12 @@ var combine = function(n, k) {
   var res = []
   var func = function(temp, index) {
     if (temp.length == k) {
-      res.push(temp)
+      res.push(temp.slice())
       return;
     }
     for (let i = index; i <= n; i++) {
       temp.push(i)
-      func(temp.slice(), i + 1)
+      func(temp, i + 1)
       temp.pop()
     }
   }
