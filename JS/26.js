43f3dfa69d0fad4fe4c928d77a1cb13ca96efe9b
bingfeng yu <ybf0711@gmail.com>
2020-01-20 23:56:30 +0800

    26---抄---时空性能好

--- JS/26.js
+++ JS/26.js
@@ -1,4 +1,5 @@
 /**
+ * 参考
  * @param {number[]} nums
  * @return {number}
  */
@@ -11,6 +12,23 @@ var removeDuplicates = function(nums) {
   }
   return nums.length
 }
+
+/**
+ * copy
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if(nums.length === 0) return 0;
+  var newIndex=0;
+  for(let i=0,j=nums.length;i<j;i++){
+      if(nums[newIndex]!==nums[i]){
+          newIndex++;
+          nums[newIndex]=nums[i];
+      }       
+  }
+  return newIndex+1;
+};
 
 removeDuplicates([0,0,1,1,1,2,2,3,3,4])
 
