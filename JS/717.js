d6333be59ff4b03835db04171515d4d02e2a02eb
bingfeng yu <ybf0711@gmail.com>
2020-01-26 16:06:05 +0800

    717---copy---线性扫描

--- JS/717.js
+++ JS/717.js
@@ -3,7 +3,17 @@
  * @param {number[]} bits
  * @return {boolean}
  */
-var isOneBitCharacter = function(bits) {
-  return /^(10|11|0)*0$/.test(bits.join(''));
-};
+// var isOneBitCharacter = function(bits) {
+//   return /^(10|11|0)*0$/.test(bits.join(''))
+// }
 
+
+var isOneBitCharacter = function(bits) {
+  var i = 0
+  while (i < bits.length - 1) {
+    i += bits[i] + 1
+  }
+  return i === bits.length - 1
+}
+
+isOneBitCharacter([1,1,1,0])
