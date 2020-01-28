10849d537c1d0ba1f8280f639a4875c174365d2e
bingfeng yu <ybf0711@gmail.com>
2020-01-28 18:49:56 +0800

    989---copy改---性能棒

--- JS/989.js
+++ JS/989.js
@@ -1,19 +1,100 @@
 /**
+ * 自己 ---没有考虑完整
  * @param {number[]} A
  * @param {number} K
  * @return {number[]}
  */
+// var addToArrayForm = function(A, K) {
+//   var temp = K.toString()
+//     .split('')
+//     .map(e => parseInt(e), 10)
+//   var lenA = A.length
+//   var lenT = temp.length
+//   for (let i = 1; i <= temp.length; i++) {
+//     A[lenA - i] += temp[lenT - i]
+//   }
+//   for (let i = lenA - 1; i >= 0; i--) {
+//     if (A[i] > 9) {
+//       A[i] -= 10
+//       if (i === 0) {
+//         A.unshift(1)
+//       } else {
+//         A[i - 1]++
+//       }
+//     }
+//   }
+//   return A
+// }
+
+/**
+ * copy
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number[]}
+ */
+
 var addToArrayForm = function(A, K) {
-  var a=A.join('')
-  var res = parseInt(a,10)
-  console.log("9007199254740992".length);
-  var test = res.toString().split('')
-  return test.map(e => parseInt(e, 10))
+  if (!K) return A
+
+  K = String(K).split('')
+  let index = A.length - 1,
+    carry = 0
+  while (K.length > 0 || carry > 0) {
+    let k = K.length > 0 ? Number(K.pop()) : 0
+    if (index === -1) {
+      A.unshift(0)
+      index++
+    }
+
+    let sum = k + A[index] + carry
+    if (sum > 9) {
+      sum = String(sum)
+      carry = Number(sum.slice(0, sum.length - 1))
+      sum = Number(sum[sum.length - 1])
+    } else {
+      carry = 0
+    }
+
+    A[index] = sum
+    index--
+  }
+
+  if (carry !== 0) {
+    A.unshift(carry)
+  }
+
+  return A
 }
 
-console.log(
-  addToArrayForm(
-    [1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3],
-    516
-  )
-)
+/**
+ * copy改---性能棒
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number[]}
+ */
+
+var addToArrayForm = function(A, K) {
+  if (!K) return A
+
+  K = String(K).split('')
+  let index = A.length - 1,
+    carry = 0
+  while (K.length > 0 || carry > 0) {
+    let k = K.length > 0 ? Number(K.pop()) : 0
+    if (index === -1) {
+      A.unshift(0)
+      index++
+    }
+    let sum = k + A[index] + carry
+    if (sum > 9) {
+      carry = 1
+      sum -= 10
+    } else {
+      carry = 0
+    }
+    A[index] = sum
+    index--
+  }
+  return A
+}
+console.log(addToArrayForm([9, 9, 9, 9], 34))
