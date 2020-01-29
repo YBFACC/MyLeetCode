cc4d44df04d7bcfe1f53e13c1585bea752612d64
bingfeng yu <ybf0711@gmail.com>
2020-01-29 20:13:53 +0800

    303---参考---暴力---性能可以

---
+++ JS/303.js
@@ -0,0 +1,26 @@
+/**
+ * 参考---暴力---性能可以
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+  this.nums = nums
+}
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+  let sum = 0
+  for (let k = i; k <= j; k++) {
+    sum += this.nums[k]
+  }
+  return sum
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
