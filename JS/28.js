f69c52fa9afa41caaa6ff41654f02fc9a9ed97c1
bingfeng yu <ybf0711@gmail.com>
2020-02-01 19:05:08 +0800

    28---自己--简单---性能棒

--- JS/28.js
+++ JS/28.js
@@ -1,21 +1,22 @@
 /**
+ * 自己--简单---性能棒
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 var strStr = function(haystack, needle) {
   var len = needle.length
+  if (len === 0) return 0
   for (let i = 0; i < haystack.length; i++) {
-    let temp = haystack.slice(i,i+len)
-    if(temp===needle){
+    let temp = haystack.slice(i, i + len)
+    if (temp === needle) {
       return i
     }
-    if(i>haystack.length-len){
+    if (i > haystack.length - len) {
       return -1
     }
   }
-
-  return 0
+  return -1
 }
 
-strStr('hello', 'll')
+console.log(strStr('', ''))
