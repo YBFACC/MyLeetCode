8f4c117d0979a6d2ab00fba7d12b5c9846229326
bingfeng yu <ybf0711@gmail.com>
2020-02-05 16:30:22 +0800

    1071---copy---跟914题的求公因子gcd算法一样--性能好

--- JS/1071.js
+++ JS/1071.js
@@ -20,6 +20,19 @@ var gcdOfStrings = function(str1, str2) {
 }
 
 
+/**
+ * copy---跟914题的求公因子gcd算法一样--性能好
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+var gcdOfStrings = function(str1, str2) {
+  if (str1 + str2 !== str2 + str1) return ''
+  const gcd = (a, b) => (0 === b ? a : gcd(b, a % b))
+  return str1.substring(0, gcd(str1.length, str2.length))
+};
+
 
 
 console.log(
