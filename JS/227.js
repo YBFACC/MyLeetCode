b87f490581980604735a762c2b4c52f346650816
bingfeng yu <ybf0711@gmail.com>
2020-02-14 17:53:59 +0800

    227--参考思路--空间性能差--改后提升了时间性能

--- JS/227.js
+++ JS/227.js
@@ -1,5 +1,5 @@
 /**
- * 参考思路--空间性能差
+ * 参考思路--空间性能差---改后提升了时间性能
  * @param {string} s
  * @return {number}
  */
@@ -7,28 +7,27 @@ var calculate = function(s) {
   let sign = s.match(/\+|\-|\*|\//g)
   let num = s.match(/\d+/g)
   let arr = []
-  arr.push(Number(num[0]))
+  arr.push(num[0])
   for (let i = 1; i < num.length; i++) {
-    let number = Number(num[i])
     if (sign[i - 1] === '+' || sign[i - 1] === '-') {
       arr.push(sign[i - 1])
-      arr.push(number)
+      arr.push(num[i])
     } else if (sign[i - 1] === '*') {
       let temp = arr.pop()
-      temp = temp * number
+      temp = temp * num[i]
       arr.push(temp)
     } else if (sign[i - 1] === '/') {
       let temp = arr.pop()
-      temp = Math.trunc(temp / number)
+      temp = Math.trunc(temp / num[i])
       arr.push(temp)
     }
   }
-  let count = arr[0]
+  let count = Number(arr[0])
   for (let i = 1; i < arr.length; i += 2) {
     if (arr[i] === '+') {
-      count += arr[i + 1]
+      count += Number(arr[i + 1])
     } else if (arr[i] === '-') {
-      count -= arr[i + 1]
+      count -= Number(arr[i + 1])
     }
   }
   return count
