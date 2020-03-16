ece8df7e1cb7874ea52452dc6dcd9dfd5f360151
bingfeng yu <ybf0711@gmail.com>
2020-03-16 17:52:13 +0800

    建议练各种排序

---
+++ JS/912-排序数组-medium.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=912 lang=javascript
+ *
+ * [912] 排序数组
+ */
+
+// @lc code=start
+/**
+ * 建议练各种排序
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+  return nums.sort((a, b) => a - b)
+}
+// @lc code=end
