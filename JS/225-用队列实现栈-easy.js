6b71a871812215b5d94a2ad930918efbabe5d8b7
bingfeng yu <ybf0711@gmail.com>
2020-03-23 14:31:59 +0800

    自己--简单题

---
+++ JS/225-用队列实现栈-easy.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=225 lang=javascript
+ *
+ * [225] 用队列实现栈
+ */
+
+// @lc code=start
+/**
+ * 自己--简单题
+ * Initialize your data structure here.
+ */
+var MyStack = function() {
+  this.list = []
+}
+
+/**
+ * Push element x onto stack.
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+  this.list.push(x)
+}
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+  return this.list.pop()
+}
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+  return this.list[this.list.length - 1]
+}
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+  return this.list.length === 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
