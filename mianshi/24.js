13361fba817a9eb2494b0af48971885a6e37074d
bingfeng yu <ybf0711@gmail.com>
2020-03-30 00:58:49 +0800

    参考--双指针法

--- mianshi/24.js
+++ mianshi/24.js
@@ -22,3 +22,21 @@ var reverseList = function(head) {
   }
   return list[list.length - 1]
 }
+
+/**
+ * 参考--双指针法
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let pre = head
+  let cur = null
+  while (pre) {
+    let temp = pre.next
+    pre.next = cur
+    cur = pre
+    pre = temp
+  }
+
+  return cur
+}
