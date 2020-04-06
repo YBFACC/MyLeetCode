11861f64ed9ceb96f307a78d8d99465bfa3a4314
bingfeng yu <ybf0711@gmail.com>
2020-04-06 22:01:30 +0800

    自己--以前写对的现在又忘了

---
+++ mianshi/49.js
@@ -0,0 +1,38 @@
+/**
+ * 自己--以前写对的现在又忘了
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function (n) {
+  let dp = [1, 2, 3, 4, 5]
+  let index2 = 2
+  let index3 = 2
+  let index5 = 4
+  let mul2 = 6
+  let mul3 = 9
+  let mul5 = 25
+  while (dp.length < n) {
+    let min = Math.min(mul2, mul3, mul5)
+    if (mul2 === min) {
+      dp.push(mul2)
+    } else if (mul3 === min) {
+      dp.push(mul3)
+    } else if (mul5 === min) {
+      dp.push(mul5)
+    }
+    let end = dp[dp.length - 1]
+    while (mul5 <= end) {
+      index5++
+      mul5 = 5 * dp[index5]
+    }
+    while (mul3 <= end) {
+      index3++
+      mul3 = 3 * dp[index3]
+    }
+    while (mul2 <= end) {
+      index2++
+      mul2 = 2 * dp[index2]
+    }
+  }
+  return dp[n - 1]
+}
