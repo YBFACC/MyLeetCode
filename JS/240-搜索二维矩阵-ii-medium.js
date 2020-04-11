ca2a5c1c8bf913336762ae24b1221d00fa6ae3f9
bingfeng yu <ybf0711@gmail.com>
2020-04-11 21:21:38 +0800

    同面试04-74题

---
+++ JS/240-搜索二维矩阵-ii-medium.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=javascript
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+/**
+ * 同面试04-74题
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  if (matrix.length < 1) return false
+  let row = matrix.length
+  let col = matrix[0].length
+  for (let i = 0; i < row; i++) {
+    if (matrix[i][0] > target) {
+      break
+    } else if (matrix[i][0] === target) {
+      return true
+    }
+    for (let j = 0; j < col; j++) {
+      if (matrix[0][j] > target) {
+        break
+      } else if (matrix[0][j] === target) {
+        return true
+      }
+      if (matrix[i][j] === target) {
+        return true
+      }
+    }
+  }
+  return false
+}
+// @lc code=end
