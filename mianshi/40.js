b6f0bf658d641d7093be70e94b7d8138f78fef2f
bingfeng yu <ybf0711@gmail.com>
2020-04-30 12:52:43 +0800

    自己重新写大顶堆

--- mianshi/40.js
+++ mianshi/40.js
@@ -1,56 +1,82 @@
 /**
- * 自己--简单法
+ * 自己重新写大顶堆
  * @param {number[]} arr
  * @param {number} k
  * @return {number[]}
  */
-var getLeastNumbers = function(arr, k) {
-  arr.sort((a, b) => a - b)
-  return arr.slice(0, k)
-}
-
-/**
- * 快排
- *
- * 每次从[start, end]范围内的数组中随机选择一个标杆元素(代码里取得是第一个元素),
- * 然后把数组中所有小于标杆的放在数组左边，所有大于标杆的元素放在数组右边，
- * 然后判断标杆元素的位置是否等于目标位置。如果目标位置小于当前位置，则继续在左边查找，
- * 如果目标位置大于当前位置，则继续在右边查找。这样每次迭代都会缩小查找的范围。
- * 最理想的情况下时间复杂度是 O(logN)
- * @param {number[]} arr
- * @param {number} k
- * @return {number[]}
- */
-var getLeastNumbers = function(arr, k) {
-  let len = arr.length
-  if (!len || !k) return []
-  let start = 0
-  let end = len - 1
-  // 寻找一次标杆元素的位置
-  let index = quikSort(arr, start, end)
-  // 如果标杆元素的位置不等于 K
-  while (index !== k - 1) {
-    if (index > k - 1) {
-      // 如果上一次查找，标杆元素位置大于目标位置
-      end = index - 1
-      index = quikSort(arr, start, end)
-    } else {
-      // 如果上一次查找，标杆元素位置小于目标位置
-      start = index + 1
-      index = quikSort(arr, start, end)
+var getLeastNumbers = function (arr, k) {
+  const length = arr.length
+  if (k >= length) {
+    return arr
+  }
+  const heap = new MaxHeap(arr.slice(0, k))
+  for (let i = k; i < length; ++i) {
+    if (heap.top() > arr[i]) {
+      heap.extract()
+      heap.insert(arr[i])
     }
   }
-  return arr.slice(0, index + 1)
+  return heap.container
 }
 
-function quikSort(arr, left, right) {
-  let pivot = arr[left]
-  while (left < right) {
-    while (left < right && arr[right] >= pivot) right--
-    arr[left] = arr[right]
-    while (left < right && arr[left] < pivot) left++
-    arr[right] = arr[left]
+class MaxHeap {
+  constructor(arr = []) {
+    this.container = []
+    if (Array.isArray(arr)) {
+      arr.forEach(this.insert.bind(this))
+    }
   }
-  arr[left] = pivot
-  return left
+
+  insert(data) {
+    const { container } = this
+
+    container.push(data)
+    let index = container.length - 1
+    while (index) {
+      let parent = Math.floor((index - 1) / 2)
+      if (container[index] <= container[parent]) {
+        break
+      }
+      swap(container, index, parent)
+      index = parent
+    }
+  }
+
+  extract() {
+    const { container } = this
+    if (!container.length) {
+      return null
+    }
+
+    swap(container, 0, container.length - 1)
+    const res = container.pop()
+    const length = container.length
+    let index = 0,
+      exchange = index * 2 + 1
+
+    while (exchange < length) {
+      // 如果有右节点，并且右节点的值大于左节点的值
+      let right = index * 2 + 2
+      if (right < length && container[right] > container[exchange]) {
+        exchange = right
+      }
+      if (container[exchange] <= container[index]) {
+        break
+      }
+      swap(container, exchange, index)
+      index = exchange
+      exchange = index * 2 + 1
+    }
+
+    return res
+  }
+
+  top() {
+    if (this.container.length) return this.container[0]
+    return null
+  }
+}
+
+function swap(arr, i, j) {
+  ;[arr[i], arr[j]] = [arr[j], arr[i]]
 }
