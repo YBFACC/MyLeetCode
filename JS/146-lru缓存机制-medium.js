f9fdffb7828dfb1b26797c4b629f4cbb427714d1
bingfeng yu <ybf0711@gmail.com>
2020-06-01 02:45:42 +0800

    参考--map+双向队列

--- JS/146-lru缓存机制-medium.js
+++ JS/146-lru缓存机制-medium.js
@@ -39,7 +39,71 @@ LRUCache.prototype.put = function (key, value) {
 }
 // @lc code=end
 
+//参考--map+双向队列
+class ListNode {
+  constructor(key, value) {
+    this.key = key
+    this.value = value
+    this.next = null
+    this.pre = null
+  }
+}
 
+class LRUCache {
+  constructor(cache) {
+    this.cache = cache
+    this.count = 0
+    this.map = {}
+    this.dummyHead = new ListNode()
+    this.dummyTail = new ListNode()
+    this.dummyHead.next = this.dummyTail
+    this.dummyTail.prev = this.dummyHead
+  }
+  get(key) {
+    let node = this.map[key]
+    if (!node) return -1
+    this.moveToHead(node)
+    return node.value
+  }
+  put(key, value) {
+    let node = this.map[key]
+    if (!node) {
+      let newNode = new ListNode(key, value)
+      this.map[key] = newNode
+      this.addToHead(newNode)
+      this.count++
+      if (this.count > this.cache) {
+        this.removeLater()
+      }
+    } else {
+      node.value = value
+      this.moveToHead(node)
+    }
+  }
+  moveToHead(node) {
+    this.removeFromList(node)
+    this.addToHead(node)
+  }
+  removeFromList(node) {
+    let temp_pre = node.pre
+    let temp_next = node.next
+    temp_pre.next = temp_next
+    temp_next.pre = temp_pre
+  }
+  addToHead(node) {
+    node.pre = this.dummyHead
+    node.next = this.dummyHead.next
+
+    this.dummyHead.next.pre = node
+    this.dummyHead.next = node
+  }
+  removeLater() {
+    let later = this.dummyTail.pre
+    this.removeFromList(later)
+    delete this.map[later.key]
+    this.count--
+  }
+}
 
 let cache = new LRUCache(2)
 cache.put(1, 1)
