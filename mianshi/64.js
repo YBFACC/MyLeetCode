300ee6f2e3152996da44e81f624a16e3f70da492
bingfeng yu <ybf0711@gmail.com>
2020-06-02 00:15:51 +0800

    0的隐式转化为false

--- mianshi/64.js
+++ mianshi/64.js
@@ -14,3 +14,14 @@ var sumNums = function (n) {
 }
 
 sumNums(3)
+
+//0的隐式转化为false
+var sumNums = function (n) {
+  let res = 0
+  const func = function (num) {
+    res += num--
+    num && func(num)
+  }
+  func(n)
+  return res
+}
