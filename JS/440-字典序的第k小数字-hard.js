3e7e170ae3e06cbdc5187e4e69a37b3b5cc29608
bingfeng yu <ybf0711@gmail.com>
2020-06-02 22:39:40 +0800

    copy--牛皮

---
+++ JS/440-字典序的第k小数字-hard.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=440 lang=javascript
+ *
+ * [440] 字典序的第K小数字
+ */
+
+// @lc code=start
+
+/**
+ * copy--牛皮
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findKthNumber = function (n, k) {
+  let getCount = (prefix, n) => {
+    let count = 0
+    for (let cur = prefix, next = prefix + 1; cur <= n; cur *= 10, next *= 10)
+      count += Math.min(next, n + 1) - cur
+    return count
+  }
+  let p = 1//累计到K
+  let prefix = 1//前缀匹配
+  while (p < k) {
+    let count = getCount(prefix, n)//下一个区间有多少个数
+    if (p + count > k) {
+      prefix *= 10
+      p++
+    } else if (p + count <= k) {
+      prefix++
+      p += count
+    }
+  }
+  return prefix
+}
+
+// @lc code=end
+findKthNumber(100, 30)
