5f487013760c1cec90334f5514702e6256fc408a
bingfeng yu <ybf0711@gmail.com>
2020-06-12 15:33:14 +0800

    参考--2维dp--最长公共子序列

---
+++ JS/1143-最长公共子序列-medium.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=javascript
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+/**
+ * 参考--2维dp--最长公共子序列
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const m = text1.length
+  const n = text2.length
+  let dp = []
+  for (let i = 0; i <= m; i++) {
+    dp[i] = []
+    for (let j = 0; j <= n; j++) {
+      if (!i || !j) dp[i][j] = 0
+      else if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j])
+      }
+    }
+  }
+  return dp[m][n]
+}
+// @lc code=end
