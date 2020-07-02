7c93fa4d6274360ede66e0d02355b438937e8357
bingfeng yu <ybf0711@gmail.com>
2020-07-02 15:31:19 +0800

    copy--依次两两归并

--- JS/378-有序矩阵中第k小的元素-medium.js
+++ JS/378-有序矩阵中第k小的元素-medium.js
@@ -69,6 +69,36 @@ function swap(list, i, j) {
 }
 // @lc code=end
 
+/**
+ * copy--依次两两归并
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (matrix, k) {
+  if (matrix.length < 1) return 0
+  let arr = matrix.reduce((a, b) => merge(a, b))
+  return arr[k - 1]
+}
+
+function merge(left, right) {
+  let llen = left.length
+  let rlen = right.length
+  let i = 0
+  let j = 0
+  let res = []
+  while (i < llen && j < rlen) {
+    if (left[i] < right[j]) {
+      res.push(left[i++])
+    } else {
+      res.push(right[j++])
+    }
+  }
+  while (i < llen) res.push(left[i++])
+  while (j < rlen) res.push(right[j++])
+  return res
+}
+
 kthSmallest(
   [
     [1, 5, 9],
