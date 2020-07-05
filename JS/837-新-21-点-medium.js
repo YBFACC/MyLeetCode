691ad77ee95a3d71c56e0b3b99513469fd91bca6
bingfeng yu <ybf0711@gmail.com>
2020-07-05 22:40:46 +0800

    参考--反推dp

--- JS/837-新-21-点-medium.js
+++ JS/837-新-21-点-medium.js
@@ -5,27 +5,34 @@
  */
 
 // @lc code=start
-//copy--
-var new21Game = function(N, K, W) {
-  const dp = {};
-  // 最后一轮抽取后得分在 K ~ k - 1 + W 中间
-  let temp = 0;
-  for (i = K; i < K + W; i ++) {
-    if (i > N) {
-      dp[i] = 0;
+/**
+ * 参考--反推dp
+ * @param {number} N
+ * @param {number} K
+ * @param {number} W
+ * @return {number}
+ */
+var new21Game = function (N, K, W) {
+  const dp = {}
+  for (let i = K; i < K + W; i++) {
+    if (i <= N) {
+      dp[i] = 1
     } else {
-      dp[i] = 1;
-    }
-    if (i !== K) {
-      temp += dp[i];
+      dp[i] = 0
     }
   }
-  dp[K + W] = dp[K] * W - temp;
-  for (i = K - 1; i >= 0; i --) {
-    dp[i] = ( dp[i + 1] * (W + 1) - dp[i + 1 + W]) / W;
+  dp[K - 1] = 0
+  for (let j = 1; j <= W; j++) {
+    dp[K - 1] += dp[j + K - 1] / W
   }
-  return dp[0];
-};
+
+  for (let i = K - 2; i >= 0; i--) {
+    dp[i] = dp[i + 1] + (-dp[i + W + 1] + dp[i + 1]) / W
+  }
+
+  return dp[0]
+}
 
 // @lc code=end
 
+new21Game(21, 17, 10)
