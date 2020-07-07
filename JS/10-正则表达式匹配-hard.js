5d6151c36197c3d1ac7cd02581d765efbb33747f
bingfeng yu <ybf0711@gmail.com>
2020-07-07 16:29:51 +0800

    参考--dp解法

--- JS/10-正则表达式匹配-hard.js
+++ JS/10-正则表达式匹配-hard.js
@@ -12,10 +12,10 @@
  * @return {boolean}
  */
 
-var isMatch = function(s, p) {
+var isMatch = function (s, p) {
   let res = []
   let length = s.length
-  const dfs = function(s, p, temp) {
+  const dfs = function (s, p, temp) {
     let indexS = 0
     let lenS = s.length
     let indexP = 0
@@ -61,3 +61,42 @@ var isMatch = function(s, p) {
   return res.includes(s)
 }
 // @lc code=end
+
+/**
+ * 参考--dp解法
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+const isMatch = (s, p) => {
+  if (s == null || p == null) return false
+  let len1 = s.length,
+    len2 = p.length
+
+  let dp = Array.from({ length: s.length + 1 }, () =>
+    Array.from({ length: p.length + 1 }, () => false)
+  )
+  dp[0][0] = true
+
+  for (let j = 1; j <= len2; j++) {
+    if (p[j - 1] === '*') dp[0][j] = dp[0][j - 2]
+  }
+
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === '.') {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else if (p[j - 1] === '*') {
+        if (s[i - 1] === p[j - 2] || p[j - 2] === '.') {
+          dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j]
+        } else {
+          dp[i][j] = dp[i][j - 2]
+        }
+      }
+    }
+  }
+
+  return dp[len1][len2]
+}
+
+console.log(isMatch('aa', 'a*'))
