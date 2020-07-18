89e541040327c4db11ac9c6a4ba2fdb8da9fa6eb
bingfeng yu <ybf0711@gmail.com>
2020-07-18 16:46:03 +0800

    自己--来自AVL树的删除思路

---
+++ JS/450-删除二叉搜索树中的节点-medium.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=javascript
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+const { TreeNode } = require('../LeetCode-Class/index.js')
+
+// @lc code=start
+/**
+ * 自己--来自AVL树的删除思路
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+  if (!root) return null
+  if (root.val > key) root.left = deleteNode(root.left, key)
+  else if (root.val < key) root.right = deleteNode(root.right, key)
+  else if (root.val === key) {
+    if (root.left === null) return root.right
+    else if (root.right === null) return root.left
+    else {
+      const next = getNext(root, key)
+      if (next) {
+        root.val = next.val
+        root.right = deleteNode(root.right, next.val)
+      } else {
+        const pre = getPre(root, key)
+        root.val = pre.val
+        root.left = deleteNode(root.left, pre.val)
+      }
+    }
+  }
+  return root
+}
+
+function getNext(root, key) {
+  let next = null
+  while (root) {
+    if (root.val <= key) root = root.right
+    else {
+      next = root
+      root = root.left
+    }
+  }
+  return next
+}
+function getPre(root, key) {
+  let next = null
+  while (root) {
+    if (root.val >= key) root = root.left
+    else {
+      next = root
+      root = root.right
+    }
+  }
+  return next
+}
+// @lc code=end
+let bst = TreeNode.create([5, 3, 6, 2, 4, null, 7])
+
+deleteNode(bst, 3)
