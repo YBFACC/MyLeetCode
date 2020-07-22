7c26a1d4a0181a53112e4c747053de24c9385e75
bingfeng yu <ybf0711@gmail.com>
2020-07-23 00:26:44 +0800

    参考--升序数组=>BST

---
+++ mianshi/04-02.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 参考--升序数组=>BST
+ * 每次都取中位数生成root，分割左右子树
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  if (nums.length === 0) return null
+  let mid = nums.length >> 1
+  const root = new TreeNode(nums[mid])
+  root.left = sortedArrayToBST(nums.slice(0, mid))
+  root.right = sortedArrayToBST(nums.slice(mid + 1))
+  return root
+}
