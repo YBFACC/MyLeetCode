dbce493360e2befc998d3e3a4c8c849e58e3ab60
bingfeng yu <ybf0711@gmail.com>
2020-07-24 00:46:40 +0800

    copy自己--以前找的规律题

---
+++ JS/1025-除数博弈-easy.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=javascript
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+/**
+ * copy自己--以前找的规律题
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function(N) {
+  return N % 2 == 0
+}
+
+// @lc code=end
+
