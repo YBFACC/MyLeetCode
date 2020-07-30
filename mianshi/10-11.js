3abfeac98b7425664bace1f403518a53933ce895
bingfeng yu <ybf0711@gmail.com>
2020-07-30 23:04:04 +0800

    参考--贪心

--- mianshi/10-11.js
+++ mianshi/10-11.js
@@ -1,24 +1,26 @@
 /**
- * 参考--排序后+在首尾交换
+ * 参考--贪心
+ * 默认使用--峰-谷-峰
+ * 峰的位置--前一个值不能大于峰
+ * 谷的位置--前一个值不能小于谷
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var wiggleSort = function (nums) {
-  nums.sort((a, b) => a - b)
-  let temp = nums.slice()
-  let left = 0
-  let right = nums.length - 1
-  let index = 0
-  while (left < right) {
-    nums[index++] = temp[right--]
-    nums[index++] = temp[left++]
-  }
-  if (nums.length % 2 === 1) {
-    nums[index] = temp[left]
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      nums[i] < nums[i - 1] ? swap(nums, i, i - 1) : null
+    } else {
+      nums[i] > nums[i - 1] ? swap(nums, i, i - 1) : null
+    }
   }
 }
 
-let a = [1, 2,3]
+function swap(list, i, j) {
+  ;[list[i], list[j]] = [list[j], list[i]]
+}
+
+let a = [1, 2, 3]
 wiggleSort(a)
 
 console.log()
