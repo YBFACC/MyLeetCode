7ac2a7a5d3dd37bce19ed6ac1b100caf88368f5e
bingfeng yu <ybf0711@gmail.com>
2020-08-04 11:45:33 +0800

    参考--找规律

---
+++ JS/292-nim-游戏-easy.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=javascript
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+/**
+ * 参考--找规律
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function (n) {
+  return n % 4 !== 0
+}
+// @lc code=end
