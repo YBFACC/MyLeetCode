c1e490e377aac540bb6eea4311b260dd51eee33a
bingfeng yu <ybf0711@gmail.com>
2020-08-07 13:45:13 +0800

    自己--easy

---
+++ JS/100-相同的树-easy.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=javascript
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * 自己--easy
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) return true
+  if (!p || !q) return false
+  return (
+    q.val === p.val &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+  )
+}
+// @lc code=end
