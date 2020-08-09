9c4c47b2ceac55267a17f2e27dc529e3191a35cd
bingfeng yu <ybf0711@gmail.com>
2020-08-09 22:06:24 +0800

    参考--优化

--- JS/115-不同的子序列-hard.js
+++ JS/115-不同的子序列-hard.js
@@ -6,7 +6,7 @@
 
 // @lc code=start
 /**
- * 自己--dp画图分析--还可以优化
+ * 参考--优化
  * @param {string} s
  * @param {string} t
  * @return {number}
@@ -19,28 +19,14 @@ var numDistinct = function (s, t) {
     Array.from({ length: col + 1 }, () => 0)
   )
 
-  for (let i = 1; i <= col; i++) {
-    if (t[0] === s[i - 1]) {
-      dp[1][i] = dp[1][i - 1] + 1
-    } else {
-      dp[1][i] = dp[1][i - 1]
-    }
+  for (let i = 0; i <= col; i++) {
+    dp[0][i] = 1
   }
-  let count = 0
-  for (let i = 2; i <= row; i++) {
-    if (t[i - 1] === t[i - 2]) {
-      count++
-    } else {
-      count = 0
-    }
+
+  for (let i = 1; i <= row; i++) {
     for (let j = 1; j <= col; j++) {
       if (s[j - 1] === t[i - 1]) {
-        if (count > 0) {
-          dp[i][j] = dp[i][j - 1]
-          count--
-        } else {
-          dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
-        }
+        dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
       } else {
         dp[i][j] = dp[i][j - 1]
       }
@@ -51,4 +37,4 @@ var numDistinct = function (s, t) {
 }
 // @lc code=end
 
-console.log(numDistinct('aaabcccc', 'aabcc'))
+console.log(numDistinct('babgbag', 'bag'))
