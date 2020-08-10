e8478b7245dc41525d078f983a4c7f2159c8d37b
bingfeng yu <ybf0711@gmail.com>
2020-08-10 23:10:58 +0800

    参考--异或优化空间

--- mianshi/17-04.js
+++ mianshi/17-04.js
@@ -1,18 +1,19 @@
 /**
- * 自己--还可以空间优化
+ * 参考--异或优化空间
  * @param {number[]} nums
  * @return {number}
  */
 var missingNumber = function (nums) {
   if (nums.length === 0) return 0
-  const list = Array.from({ length: nums.length + 1 }, () => false)
+
+  let res = 0
+
   for (let i = 0; i < nums.length; i++) {
-    list[nums[i]] = true
+    res ^= nums[i]
+    res ^= i + 1
   }
-  for (let i = 0; i < list.length; i++) {
-    if (!list[i]) return i
-  }
-  return 0
+
+  return res
 }
 
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
