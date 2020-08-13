2e342d4735a75f84f262f44834e9275459fc1bbb
bingfeng yu <ybf0711@gmail.com>
2020-08-13 13:13:47 +0800

    参考--不开空间-第一行/列进行标记

--- ts/01-08.ts
+++ ts/01-08.ts
@@ -1,25 +1,49 @@
 /**
- * 自己--使用额外空间
+ * 参考--不开空间-第一行/列进行标记
  Do not return anything, modify matrix in-place instead.
  */
 function setZeroes(matrix: number[][]): void {
-  const list: Array<Array<number>> = []
   const rowLimit = matrix.length
   const colLimit = matrix[0].length
+  let isFirstRowHasZero = false
+  let isFirstColHasZero = false
+
   for (let i = 0; i < rowLimit; i++) {
-    for (let j = 0; j < colLimit; j++) {
+    if (matrix[i][0] === 0) {
+      isFirstColHasZero = true
+    }
+  }
+  for (let j = 0; j < colLimit; j++) {
+    if (matrix[0][j] === 0) {
+      isFirstRowHasZero = true
+    }
+  }
+
+  for (let i = 1; i < rowLimit; i++) {
+    for (let j = 1; j < colLimit; j++) {
       if (matrix[i][j] === 0) {
-        list.push([i, j])
+        matrix[i][0] = 0
+        matrix[0][j] = 0
       }
     }
   }
-  while (list.length > 0) {
-    let [row, col] = list.shift() as Array<number>
-    for (let i = 0; i < colLimit; i++) {
-      matrix[row][i] = 0
+
+  for (let i = 1; i < rowLimit; i++) {
+    for (let j = 1; j < colLimit; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0
+      }
     }
+  }
+
+  if (isFirstRowHasZero) {
+    for (let j = 0; j < colLimit; j++) {
+      matrix[0][j] = 0
+    }
+  }
+  if (isFirstColHasZero) {
     for (let i = 0; i < rowLimit; i++) {
-      matrix[i][col] = 0
+      matrix[i][0] = 0
     }
   }
   return
