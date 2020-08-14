c5936c0ff5c2e0d2b3404fd73526e4b6324c0040
bingfeng yu <ybf0711@gmail.com>
2020-08-14 16:07:52 +0800

    参考--o1空间--累积卖出的最大值和买入的最大值

--- ts/714-买卖股票的最佳时机含手续费-medium.ts
+++ ts/714-买卖股票的最佳时机含手续费-medium.ts
@@ -6,21 +6,19 @@
 
 // @lc code=start
 
-/*参考--2维dp--累积卖出的最大值和买入的最大值
+/*参考--o1空间--累积卖出的最大值和买入的最大值
  *在卖出时，减去手续费
  *https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solution/si-chong-shi-xian-tu-jie-714-mai-mai-gu-piao-de-zu/
  */
 function maxProfit(prices: number[], fee: number): number {
-  const dp = Array.from({ length: prices.length }, () =>
-    Array.from({ length: 2 }, () => -Infinity)
-  )
-  dp[0][0] = 0
-  dp[0][1] = -prices[0]
+  let buy = 0
+  let sell = -prices[0]
+
   for (let i = 1; i < prices.length; i++) {
-    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
-    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+    buy = Math.max(buy, sell + prices[i] - fee)
+    sell = Math.max(sell, buy - prices[i])
   }
-  return Math.max(...dp[prices.length - 1])
+  return Math.max(buy, sell)
 }
 // @lc code=end
 
