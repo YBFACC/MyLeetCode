28986fbb9024cfb086e19b7d60ce4f48a7d6a268
bingfeng yu <ybf0711@gmail.com>
2020-08-15 22:51:27 +0800

    参考--经典交换xy

--- ts/01-09.ts
+++ ts/01-09.ts
@@ -1,14 +1,16 @@
+/**
+ * 参考--经典交换xy
+ * s1=xy
+ * s2=yx
+ * S2+S2=yxyx
+ * @param s1
+ * @param s2
+ */
 
-//自己--easy
 function isFlipedString(s1: string, s2: string): boolean {
   if (s1.length !== s2.length) return false
 
-  for (let i = 0; i <= s1.length; i++) {
-    const str = s1.slice(i) + s1.slice(0, i)
-    if (str === s2) return true
-  }
-
-  return false
+  return (s2 + s2).includes(s1)
 }
 
 console.log(isFlipedString('', ''))
