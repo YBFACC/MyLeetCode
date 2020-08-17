0ba2bdc189264bd4f0604c1546240793e1034501
bingfeng yu <ybf0711@gmail.com>
2020-08-17 23:00:25 +0800

    参考--高端位运算

--- ts/05-07.ts
+++ ts/05-07.ts
@@ -1,17 +1,6 @@
-
-//自己--傻瓜做法
+//参考--高端位运算
 function exchangeBits(num: number): number {
-  let index = 0
-  const list = new Array(32).fill(0)
-  while (num > 0) {
-    if (num % 2 === 1) list[index] = 1
-    num >>= 1
-    index++
-  }
-  for (let i = 0; i < 32; i += 2) {
-    ;[list[i], list[i + 1]] = [list[i + 1], list[i]]
-  }
-  return parseInt(list.reverse().join(''), 2)
+  return ((num & 0xaaaaaaaa) >>> 1) | ((num & 0x55555555) << 1)
 }
 
-console.log(exchangeBits(1))
+console.log(exchangeBits(10))
