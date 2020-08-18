87fe2336c2285b37253d0d0d016497e3aaed4ec3
bingfeng yu <ybf0711@gmail.com>
2020-08-18 15:28:23 +0800

    参考--链表--中序-转化为平衡BST

--- ts/109.ts
+++ ts/109.ts
@@ -1,23 +1,29 @@
 import { ListNode, TreeNode } from 'leetcode-class'
 
-//参考--好理解的双指针法
+//参考--链表--中序-转化为平衡BST
 function sortedListToBST(head: ListNode | null): TreeNode | null {
   if (!head) return null
-  let slow: ListNode | null = head
-  let fast: ListNode | null = head
-  let preslow = null
-  while (fast && fast.next && slow && slow.next) {
-    preslow = slow
-    slow = slow.next
-    fast = fast.next.next
-  }
-  const root = new TreeNode(slow?.val)
-
-  if (preslow !== null) {
-    preslow.next = null
-    root.left = sortedListToBST(head)
+  let Len = 0
+  let h: ListNode | null = head
+  while (head) {
+    head = head.next
+    Len++
   }
 
-  root.right = sortedListToBST(slow?.next)
-  return root
+  const dfs = function (start: number, end: number): TreeNode | null {
+    if (start > end) return null
+    const mid = (start + end) >>> 1
+    const left = dfs(start, mid - 1)
+
+    const root = new TreeNode(h?.val)
+
+    h = (h as ListNode).next
+    root.left = left
+
+    root.right = dfs(mid + 1, end)
+    return root
+  }
+  return dfs(0, Len - 1)
 }
+
+console.log(sortedListToBST(ListNode.create([-10, -3, 0, 5, 9])))
