3fe94d0ffac1d07d2c47c98a78b57dd2af02a2e9
bingfeng yu <ybf0711@gmail.com>
2020-09-01 22:29:28 +0800

    参考-同486题-二维dp

--- JS/877.js
+++ JS/877.js
@@ -1,20 +1,71 @@
-var stoneGame = function(piles) {
-  const N = piles.length;
-  // Make a (N+2) by (N+2) array, initialized with 0s.
-  const dp = Array(N + 2).fill(0).map(() => Array(N + 2).fill(0));
+//参考-同486题-二维dp
+var stoneGame = function (piles) {
+	const Len = piles.length
+	const dp = Array.from({ length: piles.length }, () =>
+		Array.from({ length: piles.length }, () => 0)
+	)
+	for (let i = 0; i < piles.length; i++) {
+		dp[i][i] = piles[i]
+	}
+	for (let i = Len - 2; i > 0; i--) {
+		for (let j = 0; j < piles.length; j++) {
+			dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+		}
+	}
+	return dp[0][piles.length - 1] >= 0
+}
 
-  // dp[i+1][j+1] = the value of the game [piles[i], ..., piles[j]]
-  for (let size = 1; size <= N; ++size)
-      for (let i = 0, j = size - 1; j < N; ++i, ++j) {
-          let parity = (j + i + N) % 2;  // j - i - N; but +x = -x (mod 2)
-          if (parity == 1)
-              dp[i+1][j+1] = Math.max(piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j]);
-          else
-              dp[i+1][j+1] = Math.min(-piles[i] + dp[i+2][j+1], -piles[j] + dp[i+1][j]);
-      }
-
-  return dp[1][N] > 0;
-};
-
-var piles = [5, 3, 4, 5]
-console.log(new stoneGame(piles))
+console.log(
+	stoneGame([
+		7,
+		7,
+		12,
+		16,
+		41,
+		48,
+		41,
+		48,
+		11,
+		9,
+		34,
+		2,
+		44,
+		30,
+		27,
+		12,
+		11,
+		39,
+		31,
+		8,
+		23,
+		11,
+		47,
+		25,
+		15,
+		23,
+		4,
+		17,
+		11,
+		50,
+		16,
+		50,
+		38,
+		34,
+		48,
+		27,
+		16,
+		24,
+		22,
+		48,
+		50,
+		10,
+		26,
+		27,
+		9,
+		43,
+		13,
+		42,
+		46,
+		24
+	])
+)
