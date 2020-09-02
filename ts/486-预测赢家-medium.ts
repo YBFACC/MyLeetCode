64025987b608ce286a3934320ab3c21acc37412d
bingfeng yu <ybf0711@gmail.com>
2020-09-02 15:02:33 +0800

    参考--博弈类dp通用解法

--- ts/486-预测赢家-medium.ts
+++ ts/486-预测赢家-medium.ts
@@ -5,22 +5,28 @@
  */
 
 // @lc code=start
-//参考--二维dp
+//参考--博弈类dp通用解法
 function PredictTheWinner(nums: number[]): boolean {
   const Len = nums.length
-  const dp = Array.from({ length: Len }, () => Array.from({ length: Len }, () => 0))
-  for (let i = 0; i < nums.length; i++) {
-    dp[i][i] = nums[i]
+  const dp = Array.from({ length: Len }, () => Array.from({ length: Len }, () => [0, 0]))
+  for (let i = 0; i < Len; i++) {
+    dp[i][i][0] = nums[i]
   }
-
   for (let i = Len - 2; i >= 0; i--) {
     for (let j = i + 1; j < Len; j++) {
-      dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1])
+      const left = nums[i] + dp[i + 1][j][1]
+      const right = nums[j] + dp[i][j - 1][1]
+      if (left > right) {
+        dp[i][j][0] = left
+        dp[i][j][1] = dp[i + 1][j][0]
+      } else {
+        dp[i][j][0] = right
+        dp[i][j][1] = dp[i][j - 1][0]
+      }
     }
   }
-  return dp[0][Len - 1] >= 0
+  return dp[0][Len - 1][0] >= dp[0][Len - 1][1]
 };
 // @lc code=end
 
 
-console.log(PredictTheWinner([1, 5, 233, 7]))
