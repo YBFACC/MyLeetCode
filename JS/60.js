5d645e6cc9510ae323bcf9eddb26265dce2fb1be
bingfeng yu <ybf0711@gmail.com>
2020-09-05 00:22:45 +0800

    重做--回溯做-大概率超时

--- JS/60.js
+++ JS/60.js
@@ -1,28 +1,29 @@
 /**
+ * 重做--回溯做-大概率超时
  * @param {number} n
  * @param {number} k
  * @return {string}
  */
-var getPermutation = function(n, k) {
-  var darr = []
-  for (let i = 1; i <= n; i++) {
-    darr.push(i.toString())
-  }
-  var res = []
-  var func = function(temp) {
-    if (temp.length == n) {
-      res.push(temp)
-      return
-    }
-
-    for (let i = 0; i < n; i++) {
-      if (!temp.includes(darr[i])) {
-        func(temp + darr[i])
-      }
-    }
-  }
-  func('')
-  return res[k-1]
+var getPermutation = function (n, k) {
+	var res = []
+	const visited = new Set()
+	var func = function (temp) {
+		if (temp.length == n) {
+			res.push(temp.join(''))
+			return
+		}
+		for (let i = 1; i <= n; i++) {
+			if (!visited.has(i)) {
+				temp.push(i)
+				visited.add(i)
+				func(temp)
+				temp.pop()
+				visited.delete(i)
+			}
+		}
+	}
+	func([])
+	return res[k - 1]
 }
 
-getPermutation(3, 3)
+console.log(getPermutation(8, 15198))
