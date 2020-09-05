d890dcbb125709cf478c3af5a59d44456cfc7a6b
bingfeng yu <ybf0711@gmail.com>
2020-09-05 15:09:16 +0800

    自己--根据k算出每个位置的字母

--- JS/60.js
+++ JS/60.js
@@ -1,29 +1,42 @@
-/**
- * 重做--回溯做-大概率超时
- * @param {number} n
- * @param {number} k
- * @return {string}
- */
+//自己--根据k算出每个位置的字母
 var getPermutation = function (n, k) {
-	var res = []
-	const visited = new Set()
-	var func = function (temp) {
-		if (temp.length == n) {
-			res.push(temp.join(''))
-			return
-		}
-		for (let i = 1; i <= n; i++) {
-			if (!visited.has(i)) {
-				temp.push(i)
-				visited.add(i)
-				func(temp)
-				temp.pop()
-				visited.delete(i)
-			}
-		}
+	let list = []
+	for (let i = 1; i <= n; i++) {
+		list.push(i)
 	}
-	func([])
-	return res[k - 1]
+  let res = ''
+  
+	k--    //索引从0开始
+	while (list.length > 1) {
+		if (k === 0) {
+			res += list.join('')
+			return res
+		}
+		let item = Math.floor(getAll(list.length) / list.length)//这个位置上字母出现的个数
+
+		let index = Math.floor(k / item)//确定他是list的哪个位置
+		k -= index * item//更新k
+		res += list.splice(index, 1)[0]//从list取出字母
+	}
+	res += list[0]
+	return res
+}
+
+function getAll(n) {
+	let all = 1
+	for (let i = n; i > 1; i--) {
+		all *= i
+	}
+	return all
 }
 
 console.log(getPermutation(8, 15198))
+//21
+console.log(getPermutation(2, 2))
+//"213"
+console.log(getPermutation(3, 3))
+
+//"312"
+console.log(getPermutation(3, 5))
+//"2314"
+console.log(getPermutation(4, 9))
