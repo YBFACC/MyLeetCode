5ca3999e219dba4bab54be24daba720a23e5faa2
bingfeng yu <ybf0711@gmail.com>
2020-09-08 05:54:03 +0800

    自己--重做--kmp

--- JS/28.js
+++ JS/28.js
@@ -1,22 +1,39 @@
 /**
- * 自己--简单---性能棒
+ * 自己--重做--kmp
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-  var len = needle.length
-  if (len === 0) return 0
-  for (let i = 0; i < haystack.length; i++) {
-    let temp = haystack.slice(i, i + len)
-    if (temp === needle) {
-      return i
-    }
-    if (i > haystack.length - len) {
-      return -1
-    }
-  }
-  return -1
+var strStr = function (haystack, needle) {
+	var len = needle.length
+	if (len === 0) return 0
+
+	return kmp(haystack, needle)
 }
 
-console.log(strStr('', ''))
+function kmp(text, pattern) {
+	const next = [-1]
+	const pLen = pattern.length
+	const tLen = text.length
+	for (let i = 0, n = -1; i < pLen; ) {
+		if (n < 0 || pattern[i] === pattern[n]) {
+			next[++i] = ++n
+		} else {
+			n = next[n]
+		}
+	}
+	for (let ti = 0, pi = 0; pLen - pi <= tLen - ti; ) {
+		if (pi < 0 || text[ti] === pattern[pi]) {
+			ti++
+			pi++
+		} else {
+			pi = next[pi]
+		}
+		if (pi === pLen) {
+			return ti - pLen
+		}
+	}
+	return -1
+}
+
+console.log(strStr('mississippi', 'issip'))
