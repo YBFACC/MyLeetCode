4a783bedcb510d895b04e2823d54a2cbd90027d7
bingfeng yu <ybf0711@gmail.com>
2020-09-19 09:26:41 +0800

    自己--博弈类-dp改DFS--剪枝

--- ts/1140-石子游戏-ii-medium.ts
+++ ts/1140-石子游戏-ii-medium.ts
@@ -5,24 +5,30 @@
  */
 
 // @lc code=start
-//参考--博弈类dp
+//自己--博弈类-dp改DFS--剪枝
 function stoneGameII(piles: number[]): number {
   const Len = piles.length
-  const dp = Array.from({ length: Len }, () => Array.from({ length: Len + 1 }, () => 0))
-  let sum = 0
+  const list = Array.from({ length: Len + 1 }, () => 0)
+
   for (let i = Len - 1; i >= 0; i--) {
-    sum += piles[i]
-    for (let m = 1; m < Len; m++) {
-      if (i + m * 2 >= Len) {
-        dp[i][m] = sum
-      } else {
-        for (let x = 1; x <= 2 * m; x++) {
-          dp[i][m] = Math.max(dp[i][m], sum - dp[i + x][Math.max(m, x)])
-        }
-      }
-    }
+    list[i] += list[i + 1] + piles[i]
   }
-  return dp[0][1]
+  const map = new Map()
+  const dfs = function (list: number[], index: number, m: number) {
+    const path = `${index},${m}`
+    if (map.has(path)) return map.get(path)
+    if (index + m * 2 >= Len) return list[index]
+    let min = Number.MAX_VALUE
+    for (let x = 1; x <= m * 2; x++) {
+      min = Math.min(min, dfs(list, index + x, Math.max(x, m)))
+    }
+    const max = list[index] - min
+    map.set(path, max)
+    return max
+  }
+
+  return dfs(list, 0, 1)
 };
+
 // @lc code=end
 console.log(stoneGameII([2, 7, 9, 4, 4]))
