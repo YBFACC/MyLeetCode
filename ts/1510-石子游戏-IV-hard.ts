32988eb75aa3a4660618761dd4b4cb6e6c2cfed3
bingfeng yu <ybf0711@gmail.com>
2020-09-20 00:34:04 +0800

    参考--false=>true的转化难想

--- ts/1510-石子游戏-IV-hard.ts
+++ ts/1510-石子游戏-IV-hard.ts
@@ -1,11 +1,12 @@
 //参考--false=>true的转化难想
+//需要从大到小
 function winnerSquareGame(n: number): boolean {
   const map = new Map()
   const dfs = function (temp: number): boolean {
     if (map.has(temp)) return map.get(temp)
     if (temp === 0) return false
-    const max = Math.sqrt(temp)
-    for (let i = 1; i <= max; i++) {
+    const max = Math.floor(Math.sqrt(temp))
+    for (let i = max; i >= 1; i--) {
       if (!dfs(temp - i * i)) {
         map.set(temp, true)
         return true
@@ -18,4 +19,4 @@ function winnerSquareGame(n: number): boolean {
 };
 
 
-console.log(winnerSquareGame(7))
+console.log(winnerSquareGame(92719))
