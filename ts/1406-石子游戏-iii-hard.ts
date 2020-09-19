b33557cbee7647869831a08f8dc28c5efd7fe61d
bingfeng yu <ybf0711@gmail.com>
2020-09-19 16:24:49 +0800

    自己--改写为DFS

--- ts/1406-石子游戏-iii-hard.ts
+++ ts/1406-石子游戏-iii-hard.ts
@@ -5,28 +5,36 @@
  */
 
 // @lc code=start
-//参考--dp-博弈类问题
+//自己--改写为DFS
 //对比上一题-中心思想一致-每次拿到最大值
 function stoneGameIII(stoneValue: number[]): string {
   const piles = stoneValue
   const Len = piles.length
-  const dp = Array.from({ length: Len + 3 }, () => 0)
-  let sum = 0
+  const list = Array.from({ length: Len + 3 }, () => 0)
   for (let i = Len - 1; i >= 0; i--) {
-    sum += piles[i]
-    dp[i] = -Infinity
-    for (let j = 1; j <= 3; j++) {
-      dp[i] = Math.max(dp[i], sum - dp[i + j])
-    }
+    list[i] = list[i + 1] + piles[i]
   }
-  if (sum === dp[0] + dp[0]) {
+  const map = new Map()
+  const dfs = function (index: number): number {
+    if (map.has(index)) return map.get(index)
+    if (index >= Len) return 0
+    let floor = -Infinity
+    for (let i = 1; i <= 3; i++) {
+      floor = Math.max(floor, list[index] - dfs(index + i))
+    }
+    map.set(index, floor)
+    return floor
+  }
+  let temp = dfs(0)
+
+  if (list[0] === temp + temp) {
     return 'Tie'
   }
-  if (sum - dp[0] > dp[0]) {
+  if (list[0] - temp > temp) {
     return 'Bob'
   }
   return "Alice"
 };
 // @lc code=end
 
-console.log(stoneGameIII([1,2,3,-9]))
+console.log(stoneGameIII([1, 2, 3, -1, -2, -3, 7]))
