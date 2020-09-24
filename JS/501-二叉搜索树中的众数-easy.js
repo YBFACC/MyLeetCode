12dd9df1f42816804c040bf1b3d6d340237cf0e8
bingfeng yu <ybf0711@gmail.com>
2020-09-24 11:29:13 +0800

    参考--改为morris--统计最大出现字母

--- JS/501-二叉搜索树中的众数-easy.js
+++ JS/501-二叉搜索树中的众数-easy.js
@@ -3,40 +3,58 @@
  *
  * [501] 二叉搜索树中的众数
  */
-
+const { TreeNode } = require('leetcode-class')
 // @lc code=start
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * 自己--map-递归--性能一般
+ * 参考--改为morris--统计最大出现字母
  * @param {TreeNode} root
  * @return {number[]}
  */
-var findMode = function(root) {
-  let map = new Map()
-  const func = function(node) {
-    if (!node) return
-    map.set(node.val, map.has(node.val) ? map.get(node.val) + 1 : 1)
-    node.left && func(node.left)
-    node.right && func(node.right)
-  }
-  func(root)
+var findMode = function (root) {
+	let count = 0,
+		maxcount = 0,
+		currValue = Infinity
+	const res = []
+	let curr = root
+	while (curr) {
+		let preLeft = curr.left
+		if (preLeft) {
+			while (preLeft.right && preLeft.right !== curr) {
+				preLeft = preLeft.right
+			}
+			if (!preLeft.right) {
+				preLeft.right = curr
+				curr = curr.left
+			} else {
+				upade(curr.val)
+				curr = curr.right
+				preLeft = null
+			}
+		} else {
+			upade(curr.val)
+			curr = curr.right
+			preLeft = null
+		}
+	}
 
-  let list = [...map]
-  list.sort((a, b) => b[1] - a[1])
-  let res = []
-  for (let i = 0; i < list.length; i++) {
-    if (i === 0 || list[i][1] === list[i - 1][1]) {
-      res.push(list[i][0])
-    } else {
-      break
-    }
-  }
-  return res
+	function upade(nodeVal) {
+		if (nodeVal === currValue) {
+			count++
+		} else {
+			count = 1
+			currValue = nodeVal
+		}
+		if (count === maxcount) {
+			res.push(nodeVal)
+		}
+		if (count > maxcount) {
+			res.length = 0
+			res.push(nodeVal)
+			maxcount = count
+		}
+	}
+	return res
 }
 // @lc code=end
+
+findMode(TreeNode.create([5, 3, 6, 2, 4, null, null, 1]))
