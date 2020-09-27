d41adb3a0eb4b938e7c9b89d16103d448363d275
bingfeng yu <ybf0711@gmail.com>
2020-09-27 21:48:31 +0800

    重做--分析二叉搜索树的情况

--- JS/235-二叉搜索树的最近公共祖先-easy.js
+++ JS/235-二叉搜索树的最近公共祖先-easy.js
@@ -6,14 +6,7 @@
 
 // @lc code=start
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * 自己--回溯法--注意形参的类型--性能差
+ * 重做--分析二叉搜索树的情况
  *
  * 看题解后：注意这是二叉搜索树有以下特点🚀🚀🚀🚀
  * 节点左子树上的所有节点的值都小于等于节点的值
@@ -25,44 +18,13 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
-  let arrP = []
-  let arrQ = []
-  let findP = false
-  let findQ = false
-  const func = function(root) {
-    if (root === null) {
-      return
-    }
-    if (root.val === p.val) {
-      findP = true
-      arrP.push(root)
-    } else if (root.val === q.val) {
-      findQ = true
-      arrQ.push(root)
-    }
-    if (!findP) {
-      arrP.push(root)
-    }
-    if (!findQ) {
-      arrQ.push(root)
-    }
-    func(root.left)
-    func(root.right)
-    if (!findP) {
-      arrP.pop()
-    }
-    if (!findQ) {
-      arrQ.pop()
-    }
-  }
-  func(root)
-  let res
-  for (let i = 0; i < arrP.length && i < arrQ.length; i++) {
-    if (arrP[i].val === arrQ[i].val) {
-      res = arrP[i]
-    }
-  }
-  return res
+var lowestCommonAncestor = function (root, p, q) {
+	if (p.val < root.val && q.val < root.val) {
+		return lowestCommonAncestor(root.left, p, q)
+	}
+	if (p.val > root.val && q.val > root.val) {
+		return lowestCommonAncestor(root.right, p, q)
+	}
+	return root
 }
 // @lc code=end
