2cfee8bccd43065bf935287f55bd67def75b70cb
bingfeng yu <ybf0711@gmail.com>
2020-10-08 15:48:03 +0800

    自己--不动脑子

---
+++ ts/344-反转字符串-easy.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=typescript
+ *
+ * [344] 反转字符串
+ */
+//自己--不动脑子
+// @lc code=start
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  s.reverse()
+  return
+};
+// @lc code=end
+
+reverseString(["h", "e", "l", "l", "o"])
