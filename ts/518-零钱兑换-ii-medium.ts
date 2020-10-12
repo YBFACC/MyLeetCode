382c677dc22ccf4ea8d491efde89eb4a3cc6ff96
bingfeng yu <ybf0711@gmail.com>
2020-10-12 21:24:32 +0800

    参考--dp--排列数与组合数的区别

--- ts/518-零钱兑换-ii-medium.ts
+++ ts/518-零钱兑换-ii-medium.ts
@@ -5,40 +5,24 @@
  */
 
 // @lc code=start
-//自己--超时
+//参考--dp--排列数与组合数的区别
 function change(amount: number, coins: number[]): number {
-  let res = 0
-  coins.sort((a, b) => a - b)
-  const Len = coins.length
-
-  const dfs = function (index: number, sum: number): boolean {
-
-    if (sum > amount) return
-    if (sum === amount) {
-      res++
-      return
+  const dp = Array.from({ length: amount + 1 }, () => 0)
+  dp[0] = 1
+  for (const coin of coins) {
+    for (let i = 0; i <= amount; i++) {
+      if (coin <= i)
+        dp[i] += dp[i - coin]
     }
-    if (index < 0) return
-
-    const item = coins[index]
-    dfs(index - 1, sum)
-    let temp = sum
-    while (temp <= amount) {
-      temp += item
-      dfs(index - 1, temp)
-    }
-
   }
-  dfs(Len - 1, 0)
-
-  return res
+  return dp[amount]
 };
 // @lc code=end
 
+console.log(change(5, [1, 2, 5]))
 change(500
   , [3, 5, 7, 8, 9, 10, 11])
 
-console.log(change(5, [1, 2, 5]))
 
 console.log(change(10, [10]))
 
