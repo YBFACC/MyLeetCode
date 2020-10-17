b0b8c6e55f7b6a8666aeaec263d72622742416ac
bingfeng yu <ybf0711@gmail.com>
2020-10-17 15:27:20 +0800

    copy--使用掩码--3进制

--- ts/137-只出现一次的数字-ii-medium.ts
+++ ts/137-只出现一次的数字-ii-medium.ts
@@ -5,21 +5,14 @@
  */
 
 // @lc code=start
-//自己--需要开一个32位数组
+//copy--使用掩码--3进制
 var singleNumber = function (nums: number[]): number {
-  const counts = new Int16Array(32);
-  for (let num of nums) {
-    for (let j = 0; j < 32; j++) {
-      counts[j] += num & 1;
-      num >>>= 1;
-    }
+  let ones = 0, twos = 0;
+  for (const num of nums) {
+    ones = ones ^ num & ~twos;
+    twos = twos ^ num & ~ones;
   }
-  let res = 0
-  for (let i = 0; i < 32; i++) {
-    res <<= 1;
-    res |= counts[31 - i] % 3;
-  }
-  return res;
+  return ones;
 };
 // @lc code=end
 
