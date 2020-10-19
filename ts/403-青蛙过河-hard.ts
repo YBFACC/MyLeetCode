07675b0f752bb26f12f680b8cac390320b99b44d
bingfeng yu <ybf0711@gmail.com>
2020-10-20 00:15:52 +0800

    参考--dp

--- ts/403-青蛙过河-hard.ts
+++ ts/403-青蛙过河-hard.ts
@@ -5,42 +5,33 @@
  */
 
 // @lc code=start
-//自己--DFS+memo
+//参考--dp
 function canCross(stones: number[]): boolean {
   const Len = stones.length
-  let res = false
-  const map = new Map()
-  const dfs = function (index: number, jump: number) {
-    if (res) return
-    if (index === Len - 1) {
-      res = true
-      return
-    }
-    const path = `${index}-${jump}`
-    if (map.has(path)) return false
-    const Max = stones[index] + jump + 1
-    let next = index
-    while (stones[++next] <= Max) {
-
-      if (stones[next] === stones[index] + jump) {
-        dfs(next, jump)
-      }
-      if (stones[next] === stones[index] + jump + 1) {
-        dfs(next, jump + 1)
-      }
-      if (stones[next] === stones[index] + jump - 1) {
-        dfs(next, jump - 1)
-      }
-    }
-    map.set(path, false)
-    return false
+  //第i个石头到第j个石头是否成功
+  //使用set存储--到第i个石头的步长
+  const dp = new Map()
+  for (const stone of stones) {
+    dp.set(stone, new Set())
   }
-  dfs(0, 0)
 
-  return res
+  dp.get(0).add(0)
+  if (stones[1] > 1) return false
+  for (let i = 0; i < Len; i++) {
+    for (const k of dp.get(stones[i])) {
+      for (let step = k - 1; step <= k + 1; step++) {
+        if (step > 0 && dp.has(stones[i] + step)) {
+          dp.get(stones[i] + step).add(step);
+        }
+      }
+    }
+
+  }
+
+  return dp.get(stones[Len - 1]).size > 0;
 };
 // @lc code=end
-
-canCross([0, 2])
-canCross([0, 1, 2, 3, 4, 8, 9, 11])
-canCross([0, 1, 3, 5, 6, 8, 12, 17])
+console.log(canCross([0, 1, 2147483647]))
+console.log(canCross([0, 2]))
+console.log(canCross([0, 1, 2, 3, 4, 8, 9, 11]))
+console.log(canCross([0, 1, 3, 5, 6, 8, 12, 17]))
