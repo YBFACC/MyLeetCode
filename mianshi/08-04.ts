e3b130448a9ad6b18b554d500db33ac5d8eca46e
bingfeng yu <ybf0711@gmail.com>
2020-10-28 23:02:46 +0800

    参考--回溯

--- mianshi/08-04.ts
+++ mianshi/08-04.ts
@@ -1,18 +1,22 @@
-//参考--在上次的结果中追加下一次的值
+//参考--回溯
+//每一个值有选和不选两种状态
 function subsets(nums: number[]): number[][] {
   const Len = nums.length
-  let res: number[][] = [[]]
-  if (Len === 0) return [[]]
+  let res: number[][] = []
 
-  for (let i = 0; i < Len; i++) {
-    let temp: number[][] = []
-    res.forEach(v => {
-      temp.push(v.slice())
-      v.push(nums[i])
-    })
-    res = [...res, ...temp]
+  const dfs = function (temp: number[], index: number) {
+    if (Len === index) {
+      res.push(temp.slice())
+      return
+    }
+    res.push(temp.slice())
+    for (let i = index; i < Len; i++) {
+      temp.push(nums[i])
+      dfs(temp, i + 1)
+      temp.pop()
+    }
   }
-
+  dfs([], 0)
   return res
 };
 subsets([1, 2, 3])
