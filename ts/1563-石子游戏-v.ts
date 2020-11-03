26d2ef65a0d03a738f71bf91510e8718fb5e66e0
bingfeng yu <ybf0711@gmail.com>
2020-11-03 20:12:02 +0800

    参考--DFS+memo

---
+++ ts/1563-石子游戏-v.ts
@@ -0,0 +1,43 @@
+//参考--DFS+memo
+function stoneGameV(stoneValue: number[]): number {
+  if (stoneValue.length <= 1) return 0
+  const Len = stoneValue.length
+  for (let i = 1; i < Len; i++) {
+    stoneValue[i] += stoneValue[i - 1]
+  }
+  const pre = stoneValue
+  const map = new Map()
+  const dfs = function (start: number, end: number, temp: number): number {
+    const path = `${start},${end}`
+    if (map.has(path)) return map.get(path)
+    if (start >= end) {
+      return 0
+    } 
+    let floor = Number.MIN_SAFE_INTEGER
+    for (let i = start; i < end; i++) {
+      const left_v = pre[i] - temp
+      const right_v = pre[end] - pre[i]
+      if (left_v < right_v) {
+        let V = dfs(start, i, start > 0 ? pre[start - 1] : 0)
+        floor = Math.max(floor, left_v + V)
+      } else if (left_v > right_v) {
+        let V = dfs(i + 1, end, pre[i])
+        floor = Math.max(floor, right_v + V)
+      } else if (left_v === right_v) {
+        let V = Math.max(dfs(start, i, start > 0 ? pre[start - 1] : 0), dfs(i + 1, end, pre[i]))
+        floor = Math.max(floor, left_v + V)
+      }
+    }
+    map.set(path, floor)
+    return floor
+  }
+  return dfs(0, Len - 1, 0)
+};
+
+//1103936
+console.log(stoneGameV([62008, 269055, 379802, 503405, 589774]))
+
+console.log(stoneGameV([39994, 3, 4, 10000, 10000, 10000, 10000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1000000]))
+
+//18
+console.log(stoneGameV([6, 2, 3, 4, 5, 5]))
