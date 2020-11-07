3dc7c0bf0d320b0b5bbeffe8677bae2b4f33b304
bingfeng yu <ybf0711@gmail.com>
2020-11-07 15:09:44 +0800

    参考--双指针

--- VIP/426-将二叉搜索树转化为排序的双向链表-mid.ts
+++ VIP/426-将二叉搜索树转化为排序的双向链表-mid.ts
@@ -1,31 +1,31 @@
 import { TreeNode, ListNode, runScript } from 'leetcode-class';
 
-//自己--中序BST--使用数组储存-简单
-
+//参考--双指针
+//head--指向头节点
+//pre--表示上一个节点
 function treeToDoublyList(root: Node | null): Node | null {
   if (!root) return null
-  if (!root.left && !root.right) {
-    root.left = root
-    root.right = root
-    return root
-  }
-  const list: Node[] = []
+  let head!: Node
+  let pre!: Node
 
-  const dfs = function (node: Node | null) {
+  const dfs = function (node: Node | null): void {
     if (!node) return
     dfs(node.left)
-    list.push(node)
+    if (pre) {
+      node.left = pre
+      pre.right = node
+    } else {
+      head = node
+    }
+    pre = node
     dfs(node.right)
   }
   dfs(root)
-  const Len = list.length
-  list[0].left = list[Len - 1]
-  list[0].right = list[1]
-  list[Len - 1].left = list[Len - 2]
-  list[Len - 1].right = list[0]
-  for (let i = 1; i < Len - 1; i++) {
-    list[i].left = list[i - 1]
-    list[i].right = list[i + 1]
-  }
-  return list[0]
+
+  head.left = pre
+  pre.right = head
+
+  return head
 };
+
+treeToDoublyList(Node.create([-1, null, 1, null, 9]))
