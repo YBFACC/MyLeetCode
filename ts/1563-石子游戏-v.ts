b0b9a956ac99b7b7b88851853147c8d6defd078a
bingfeng yu <ybf0711@gmail.com>
2020-11-26 20:28:19 +0800

    copy--2维dp

--- ts/1563-石子游戏-v.ts
+++ ts/1563-石子游戏-v.ts
@@ -1,43 +1,59 @@
-//参考--DFS+memo
+//copy--2维dp
 function stoneGameV(stoneValue: number[]): number {
-  if (stoneValue.length <= 1) return 0
-  const Len = stoneValue.length
-  for (let i = 1; i < Len; i++) {
-    stoneValue[i] += stoneValue[i - 1]
+  const N = 7
+  const s = Array.from({ length: N }, () => new Int32Array(N))
+  const g = Array.from({ length: N }, () => new Int32Array(N))
+  const f = Array.from({ length: N }, () => new Int32Array(N))
+  const mxl = Array.from({ length: N }, () => new Int32Array(N))
+  const mxr = Array.from({ length: N }, () => new Int32Array(N))
+  const n = stoneValue.length
+
+
+  for (let i = 0; i < n; i++) {
+    s[i][i] = stoneValue[i];
+    g[i][i] = i;
+    for (let j = i + 1; j < n; j++) {
+      s[i][j] = s[i][j - 1] + stoneValue[j];
+      let now = g[i][j - 1];
+      while (s[i][j] - s[i][now] > s[i][now]) {
+        now++;
+      }
+      g[i][j] = now;
+    }
   }
-  const pre = stoneValue
-  const map = new Map()
-  const dfs = function (start: number, end: number, temp: number): number {
-    const path = `${start},${end}`
-    if (map.has(path)) return map.get(path)
-    if (start >= end) {
-      return 0
-    } 
-    let floor = Number.MIN_SAFE_INTEGER
-    for (let i = start; i < end; i++) {
-      const left_v = pre[i] - temp
-      const right_v = pre[end] - pre[i]
-      if (left_v < right_v) {
-        let V = dfs(start, i, start > 0 ? pre[start - 1] : 0)
-        floor = Math.max(floor, left_v + V)
-      } else if (left_v > right_v) {
-        let V = dfs(i + 1, end, pre[i])
-        floor = Math.max(floor, right_v + V)
-      } else if (left_v === right_v) {
-        let V = Math.max(dfs(start, i, start > 0 ? pre[start - 1] : 0), dfs(i + 1, end, pre[i]))
-        floor = Math.max(floor, left_v + V)
+
+  for (let len = 1; len <= n; len++) {
+    for (let l = 0; l + len - 1 < n; l++) {
+      let r = l + len - 1;
+      let mid = g[l][r];
+      let ls = s[l][mid];
+      let rs = s[mid + 1][r];
+      if (ls == rs) {
+        f[l][r] = Math.max(f[l][r], mxl[l][mid]);
+        f[l][r] = Math.max(f[l][r], mxr[mid + 1][r]);
+      } else {
+        if (mid > l) {
+          f[l][r] = Math.max(f[l][r], mxl[l][mid - 1]);
+        }
+        if (mid < r) {
+          f[l][r] = Math.max(f[l][r], mxr[mid + 1][r]);
+        }
+      }
+      let v = f[l][r] + s[l][r];
+      if (l == r) {
+        mxl[l][r] = mxr[l][r] = v;
+      } else {
+        mxl[l][r] = Math.max(v, mxl[l][r - 1]);
+        mxr[l][r] = Math.max(v, mxr[l + 1][r]);
       }
     }
-    map.set(path, floor)
-    return floor
   }
-  return dfs(0, Len - 1, 0)
+  return f[0][n - 1];
 };
+//18
+console.log(stoneGameV([6, 2, 3, 4, 5, 5]))
 
 //1103936
 console.log(stoneGameV([62008, 269055, 379802, 503405, 589774]))
 
 console.log(stoneGameV([39994, 3, 4, 10000, 10000, 10000, 10000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1000000]))
-
-//18
-console.log(stoneGameV([6, 2, 3, 4, 5, 5]))
