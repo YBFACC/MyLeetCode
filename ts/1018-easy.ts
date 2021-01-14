16ca2a5a7d108f6c4b7aa816949079e51a702930
YBFACC <ybf0711@gmail.com>
2021-01-14 22:18:50 +0800

    自己--注意溢出

---
+++ ts/1018-easy.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1018 lang=typescript
+ *
+ * [1018] 可被 5 整除的二进制前缀
+ */
+
+//自己--注意移除
+
+// @lc code=start
+function prefixesDivBy5(A: number[]): boolean[] {
+  let temp = A[0]
+  const Len = A.length
+  const res: boolean[] = [A[0] === 0]
+  for (let i = 1; i < Len; i++) {
+    temp = ((temp << 1) + A[i]) % 10
+    res.push(temp % 5 === 0)
+  }
+
+  return res
+};
+// @lc code=end
+
+prefixesDivBy5([1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,0,0,0,1])
