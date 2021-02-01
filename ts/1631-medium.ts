6fc63b599c5c5af09f353f71b8aacef0c3ce1d4a
YBFACC <ybf0711@gmail.com>
2021-02-01 15:06:47 +0800

    参考--dijkstra最短路径

--- ts/1631-medium.ts
+++ ts/1631-medium.ts
@@ -4,78 +4,40 @@
  * [1631] 最小体力消耗路径
  */
 
-//参考--并查集/Kruskal最小生成树
+//参考--dijkstra最短路径
 
+import { AVLTree, Heap, TreeNode, ListNode, RunScript, Node, SegmentTree } from 'lc-tool';
 // @lc code=start
-
-interface obj {
-  [index: number]: number
-}
-
-class UnionFind {
-  parents: obj
-  //减少搜索深度
-  ranks: obj
-  //统计联通分量
-  count: number
-  constructor(n: number) {
-    this.parents = Array.from({ length: n }, (v, k) => k);
-    this.ranks = Array.from({ length: n }, (v, k) => 1);
-    this.count = n
-  }
-  init(x: number) {
-    if (this.parents[x] === undefined) {
-      this.parents[x] = x;
-      this.ranks[x] = 1
-    }
-  }
-  find(x: number) {
-    if (this.parents[x] !== x) {
-      this.parents[x] = this.find(this.parents[x]);
-    }
-    return this.parents[x];
-  }
-  merge(x: number, y: number): boolean {
-    this.init(x); this.init(y);
-    let rootX = this.find(x), rootY = this.find(y);
-    if (rootX === rootY) return false;
-    if (this.ranks[rootX] < this.ranks[rootY]) {
-      [rootX, rootY] = [rootY, rootX]
-    }
-    this.ranks[rootX] += this.ranks[rootY]
-    this.parents[rootY] = rootX
-    this.count--
-    return true
-  }
-}
 
 function minimumEffortPath(heights: number[][]): number {
   const Rows = heights.length, Cols = heights[0].length
-
-  const edges: number[][] = []
-  for (let i = 0; i < Rows; i++) {
-    for (let j = 0; j < Cols; j++) {
-      const id = i * Cols + j
-      if (i < Rows - 1) {
-        edges.push([id, id + Cols, Math.abs(heights[i][j] - heights[i + 1][j])])
-      }
-      if (j < Cols - 1) {
-        edges.push([id, id + 1, Math.abs(heights[i][j] - heights[i][j + 1])])
+  const Size = Rows * Cols
+  const Ways = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  const heap = new Heap<number[]>([], (a: number[], b: number[]) => {
+    return a[2] >= b[2]
+  })
+  const dist = Array.from({ length: Size }, () => Infinity)
+  const set = new Set()
+  dist[0] = 0
+  heap.insert([0, 0, 0])
+  while (!heap.isEmpty()) {
+    const [x, y, val] = heap.extract() as number[]
+    const Path = `${x}_${y}`
+    if (set.has(Path)) continue
+    if (x === Rows - 1 && y === Cols - 1) break
+    set.add(Path)
+    for (const [_x, _y] of Ways) {
+      const newX = _x + x
+      const newY = _y + y
+      const index = newX * Cols + newY
+      if (newX >= 0 && newX < Rows && newY >= 0 && newY < Cols &&
+        Math.max(val, Math.abs(heights[x][y] - heights[newX][newY])) <= dist[index]) {
+        dist[index] = Math.max(val, Math.abs(heights[x][y] - heights[newX][newY]))
+        heap.insert([newX, newY, dist[index]])
       }
     }
   }
-  edges.sort((a, b) => a[2] - b[2])
-  let res = 0
-  const UF = new UnionFind(Rows * Cols)
-  for (const [s, e, dis] of edges) {
-    if (UF.find(s) !== UF.find(e)) {
-      UF.merge(s, e)
-      res = dis
-    }
-    if (UF.find(0) === UF.find(Rows * Cols - 1)) break
-  }
-
-  return res
+  return dist[Size - 1]
 };
 // @lc code=end
 //1
