dbf37fe9944368083b51ed5711bfaf2ff2ede136
YBFACC <ybf0711@gmail.com>
2021-02-03 23:11:21 +0800

    参考--查并集

--- ts/778-hard.ts
+++ ts/778-hard.ts
@@ -4,41 +4,90 @@
  * [778] 水位上升的泳池中游泳
  */
 
-//提示--优先队列
-//每次从最小的的路径开始选择
+//参考--查并集
+//关键：grid[i][j] 是 [0, ..., N*N - 1] 的排列🚀
 
-import { AVLTree, Heap, TreeNode, ListNode, RunScript, Node, SegmentTree } from 'lc-tool';
+
 // @lc code=start
+interface obj {
+  [index: number]: number
+}
+
+class UnionFind {
+  parents: obj
+  //减少搜索深度
+  ranks: obj
+  //统计联通分量
+  count: number
+  constructor(n: number) {
+    this.parents = Array.from({ length: n }, (v, k) => k);
+    this.ranks = Array.from({ length: n }, (v, k) => 1);
+    this.count = n
+  }
+  init(x: number) {
+    if (this.parents[x] === undefined) {
+      this.parents[x] = x;
+      this.ranks[x] = 1
+    }
+  }
+  find(x: number) {
+    if (this.parents[x] !== x) {
+      this.parents[x] = this.find(this.parents[x]);
+    }
+    return this.parents[x];
+  }
+  merge(x: number, y: number): boolean {
+    this.init(x); this.init(y);
+    let rootX = this.find(x), rootY = this.find(y);
+    if (rootX === rootY) return false;
+    if (this.ranks[rootX] < this.ranks[rootY]) {
+      [rootX, rootY] = [rootY, rootX]
+    }
+    this.ranks[rootX] += this.ranks[rootY]
+    this.parents[rootY] = rootX
+    this.count--
+    return true
+  }
+}
+
 function swimInWater(grid: number[][]): number {
   const Rows = grid.length, Cols = grid[0].length
   const Ways = [[1, 0], [-1, 0], [0, -1], [0, 1]]
-  let res = 0
-  const heap = new Heap<number[]>([], (a: number[], b: number[]) => {
-    return a[2] >= b[2]
-  })
-  heap.insert([0, 0, grid[0][0]])
-  const set = new Set()
-  while (!heap.isEmpty()) {
-    const [x, y, dist] = heap.extract() as number[]
-    const Path = `${x}_${y}`
-    set.add(Path)
+  const Size = Rows * Cols
+  const list = Array.from({ length: Size }, () => 0)
+  const UF = new UnionFind(Size)
+  for (let i = 0; i < Rows; i++) {
+    for (let j = 0; j < Cols; j++) {
+      list[grid[i][j]] = getIndex(i, j)
+    }
+  }
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i]
+    const x = Math.floor(item / Rows)
+    const y = item % Rows
     for (const [_x, _y] of Ways) {
-      const newX = x + _x
-      const newY = y + _y
-      const newPath = `${newX}_${newY}`
-      if (newX >= 0 && newX < Rows && newY >= 0 && newY < Cols
-        && !set.has(newPath)
-      ) {
-        heap.insert([newX, newY, grid[newX][newY]])
+      const newX = _x + x
+      const newY = _y + y
+      if (inArea(newX, newY) && 
+      //我没想到的点，合并时只合并比自己小的节点
+      grid[newX][newY] <= i) {
+        UF.merge(item, getIndex(newX, newY))
+      }
+      if (UF.find(0) === UF.find(Size - 1)) {
+        return i
       }
     }
-    res = Math.max(res, dist)
-    if (x === Rows - 1 && y === Cols - 1) break
   }
+  return -1
 
-  return res
+  function getIndex(x: number, y: number): number {
+    return x * Rows + y
+  }
+  function inArea(x: number, y: number): boolean {
+    return x >= 0 && x < Rows && y >= 0 && y < Cols
+  }
 };
 // @lc code=end
 
-swimInWater([[0, 2], [1, 3]])
-swimInWater([[0, 1, 2, 3, 4], [24, 23, 22, 21, 5], [12, 13, 14, 15, 16], [11, 17, 18, 19, 20], [10, 9, 8, 7, 6]])
+console.log(swimInWater([[0, 2], [1, 3]]))
+console.log(swimInWater([[0, 1, 2, 3, 4], [24, 23, 22, 21, 5], [12, 13, 14, 15, 16], [11, 17, 18, 19, 20], [10, 9, 8, 7, 6]]))
