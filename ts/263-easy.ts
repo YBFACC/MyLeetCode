c185540ac50e243fc7b663d9423e005e4e0bb0b2
YBFACC <ybf0711@gmail.com>
2021-06-16 18:26:00 +0800

    自己--递归

---
+++ ts/263-easy.ts
@@ -0,0 +1,15 @@
+
+//自己--递归
+
+function isUgly(n: number): boolean {
+  if (n <= 0) return false
+  if (n === 1) return true
+  if (n % 2 === 0) {
+    return isUgly(n / 2)
+  } else if (n % 3 === 0) {
+    return isUgly(n / 3)
+  } else if (n % 5 === 0) {
+    return isUgly(n / 5)
+  }
+  return false
+};
